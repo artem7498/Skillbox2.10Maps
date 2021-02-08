88f84a1053e57efbe3e20cfbada3d95b08b93709
Artem Akopian <artem7498@gmail.com>
2021-02-08 15:08:51 +1000

    Initial Commit

---
+++ Skillbox2.10Maps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skillbox2.10Maps
+//
+//  Created by Артём on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
